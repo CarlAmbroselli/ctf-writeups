91926d57f88926b9fa37d3bb2c7a5ae65171c73a
Carl Ambroselli <git@carl-ambroselli.de>
2020-12-07 18:48:54 +0000

    Add pico ctf

---
+++ picoctf/guessing_game_1/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update
+apt-get install -y python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pwntools
---
+++ picoctf/guessing_game_1/task/README.md
@@ -0,0 +1,1 @@
+I made a simple game to show off my programming skills. See if you can beat it! vuln vuln.c Makefile nc jupiter.challenges.picoctf.org 51462
